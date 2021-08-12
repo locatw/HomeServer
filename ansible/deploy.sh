16e695372e9982b3ef3dba5f5b511a884e196f8e
locatw <dummy@github.com>
2021-08-12 11:22:05 +0000

    split hosts to production and development

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -6,12 +6,29 @@ SCRIPT_NAME=`basename $0`
 cd ${SCRIPT_DIR}
 
 function print_usage () {
-    echo "Usage: ${SCRIPT_NAME} HOST_GROUP"
+    echo "Usage: ${SCRIPT_NAME} INVENTORY HOST_GROUP"
     echo ""
+    echo "INVENTORY is a inventory to deploy."
     echo "HOST_GROUP is a host group of ansible."
 }
 
-HOST_GROUP=$1
+INVENTORY=$1
+if [ -z ${INVENTORY} ]; then
+    print_usage
+    exit 1
+elif [ ${INVENTORY} == "production" ]; then
+    read -p "You'll deploy to production. Ok? (y/N): " PRODUCTION_SELECTED
+    case "${PRODUCTION_SELECTED}" in
+        [yY]*)
+            ;;
+        *)
+            echo "Abort."
+            exit 0
+            ;;
+    esac
+fi
+
+HOST_GROUP=$2
 if [ -z ${HOST_GROUP} ]; then
     print_usage
     exit 1
@@ -23,4 +40,4 @@ if [ ! -f ${PLAYBOOK} ]; then
     exit 1
 fi
 
-ansible-playbook -i hosts --ask-become-pass ${PLAYBOOK}
+ansible-playbook -i ${INVENTORY} --ask-become-pass ${PLAYBOOK}
